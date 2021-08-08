04bb85e59f5d5c3a720a4438024ba0b6840899fd
Alesso4ka <kteranova@mail.ru>
2021-08-08 17:59:46 +0300

    Lesson 8. Task 1

---
+++ L8 T1.py
@@ -0,0 +1,16 @@
+#Задание 1. Определение количества различных подстрок с использованием хэш-функции.
+#Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
+#Требуется найти количество различных подстрок в этой строке.
+
+import hashlib
+
+string = input('Enter a string containing only small Latin letters: ')
+
+sum_substring = set()
+
+for i in range(len(string)):
+    for j in range(len(string), i, -1):
+        hash_str = hashlib.sha1(string[i:j].encode('utf-8')).hexdigest()
+        sum_substring.add(hash_str)
+
+print(f'{len(sum_substring) -1} different substrings in a string {string}')
