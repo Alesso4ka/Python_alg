22fff70ff40b18de14622c941dfe05101170ecbb
Alesso4ka <kteranova@mail.ru>
2021-08-08 17:58:06 +0300

    Lesson 7. Task 1-3

---
+++ L7 T1.py
@@ -0,0 +1,46 @@
+#Задание 1. Отсортируйте по убыванию методом "пузырька" одномерный целочисленный массив,
+# заданный случайными числами на промежутке [-100; 100).
+# Выведите на экран исходный и отсортированный массивы. Сортировка должна быть реализована в виде функции.
+# По возможности доработайте алгоритм (сделайте его умнее).
+
+from random import randint
+from timeit import timeit
+
+max_size = 100
+number_executions = 10_000
+
+
+def bubble_sort(array):
+    for i in range(len(array) - 1, 0, -1):
+        flag = True
+        for n in range(i):
+            if array[n] > array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+                flag = False
+
+        if flag == True:
+            break
+    return array
+
+
+def bubble_sort_no_smart(array):
+    for i in range(len(array) - 1, 0, -1):
+        for n in range(i):
+            if array[n] > array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+
+    return array
+
+
+numbers = [randint(-100, 100) for _ in range(max_size)]
+print(numbers)
+print(bubble_sort(numbers))
+
+time1 = timeit(f'bubble_sort({numbers})',
+              setup='from __main__ import bubble_sort',
+              number=NUMBER_EXECUTIONS)
+time2 = timeit(f'bubble_sort_no_smart({numbers})',
+              setup='from __main__ import bubble_sort_no_smart',
+              number=number_executions)
+print(time1)
+print(time2)
